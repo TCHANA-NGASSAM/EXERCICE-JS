518c822eb86d643512b436a0b446fe509e09dd13
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 16:20:04 +0200

    en cours

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,5 +1,6 @@
 function affichage(){
-let nombre1 = prompt('Entrezz un premier nombre')
-let nombre2 = prompt('Entre votre second nombre')
-r
+let nombre1 = prompt('Entrez un premier nombre')
+let nombre2 = prompt('Entrez votre second nombre')
+resultat = `La somme est ${nombre1 + nombre2}`
+alert(resultat)
 }
