265f4a0fd606e8f028f07631b009e2521cbc2fa8
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 16:30:26 +0200

    exercice terminer

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,7 @@
 function affichage(){
-let nombre1 = prompt('Entrez un premier nombre')
-let nombre2 = prompt('Entrez votre second nombre')
-resultat = `La somme est ${nombre1 + nombre2}`
+let nombre1 = parseFloat(prompt('Entrez un premier nombre'))
+let nombre2 = parseFloat(prompt('Entrez votre second nombre'))
+resultat = nombre1 + nombre2
 alert(resultat)
 }
+affichage()
