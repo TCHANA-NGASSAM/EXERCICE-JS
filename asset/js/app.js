76f0c0ee30a0e253d58e34cb4cca5463003e158d
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 11:40:55 +0200

    exercice terminer

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,23 +1,25 @@
-var quantity = 2;
-var unit = 'K';
-var result;
 
-switch (unit) {
-    case 'K':
-        result = quantity * 1024;
-        break;
-    case 'M':
-        result = quantity * 1024 * 1024;
-        break;
-    case 'G':
-        result = quantity * 1024 * 1024 * 1024;
-        break;
-    case 'T':
-        result = quantity * 1024 * 1024 * 1024 * 1024;
-        break;
-    default:
-        console.log("Unité invalide");
-        break;
+function valeurBinaire(){
+  let quantity = parseFloat(document.getElementById('numero').value);
+  let unit = document.getElementById('uniter').value;
+  let result;
+  
+  switch (unit.toUpperCase()) {
+      case 'K':
+          result = quantity * 1024;
+          break;
+      case 'M':
+          result = quantity * 1024 * 1024;
+          break;
+      case 'G':
+          result = quantity * 1024 * 1024 * 1024;
+          break;
+      case 'T':
+          result = quantity * 1024 * 1024 * 1024 * 1024;
+          break;
+      default:
+          result;
+          break;
+  }
+  document.getElementById('affichage').innerHTML = 'Votre valeur binaire est:' + result
 }
-
-console.log(result);
