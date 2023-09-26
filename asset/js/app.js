b100af115e179a1d2e3e30ab80c38df1b5e7d698
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 15:58:15 +0200

    exercice terminer

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,4 +1,9 @@
-function affichage (){
-  const display = 'Bonjour le monde'
-  document.getElementById('afficher').innerHTML = 'resultat :' + display
+// function affichage (){
+//   const display = 'Bonjour le monde'
+//   document.getElementById('afficher').innerHTML = 'resultat :' + display
+// }
+function affichage(){
+  let nom = prompt('Entre votre nom')
+  let resultat = `Bonjour,${nom}`
+  alert(resultat)
 }
