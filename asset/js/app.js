5d918037033b6807146245a2c9581d8c7b2bda1b
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 18:50:37 +0200

    exercice terminer

--- asset/js/app.js
+++ asset/js/app.js
@@ -7,18 +7,18 @@ function afficherLesNombrePremier() {
     }
   } 
   console.log(primeNumbers)
-
+document.getElementById('Afficher').innerHTML = primeNumbers 
 }
-afficherLesNombrePremier()
+
 function isPrime(number) {
   let counter = 0
   for(let i=1;i<=100;i++){
-    if (counter === 2){
-      return number
+    if (counter === 3){
+      return null
     }
     if (number % i === 0){
       counter++
     }
   }
-  return null
+  return number
 }
