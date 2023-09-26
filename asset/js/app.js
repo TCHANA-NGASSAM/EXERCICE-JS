08dd5231c300e847e830bc81b8c81d8f94b44d67
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-09-26 11:01:58 +0200

    exercice terminer

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,11 +1,20 @@
-let variable1 = 5;
-let variable2 = 8;
-let variable3 = 10;
-
-if (variable1 === variable2 && variable2 === variable3) {
-    console.log("Les trois variables sont identiques.");
-} else if (variable1 === variable2 || variable2 === variable3 || variable3 === variable1) {
-    console.log("Deux des variables sont de valeurs égales.");
-} else {
-    console.log("Les trois variables sont différentes.");
+function comparaison() {
+  let variable1 = parseFloat(document.getElementById("numero1").value);
+  let variable2 = parseFloat(document.getElementById("numero2").value);
+  let variable3 = parseFloat(document.getElementById("numero3").value);
+  let resultat;
+  switch (true) {
+    case variable1 == variable2 && variable1 == variable3:
+      resultat = "les trois variables sont identiques";
+      break;
+    case variable1 == variable2 ||
+      variable1 == variable3 ||
+      variable2 == variable3:
+      resultat = "Deux des  variables sont de valeur egal";
+      break;
+    default:
+      resultat = "les trois variables sont diffentes";
+      break;
+  }
+  document.getElementById("affichage").innerHTML = "le resultat :" + resultat;
 }
