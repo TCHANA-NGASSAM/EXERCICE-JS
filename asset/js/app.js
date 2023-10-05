9fbfe50df81ae238e281e3b77e36c3e9781a0800
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-05 12:19:49 +0200

    Exercice terminer

---
+++ asset/js/app.js
@@ -0,0 +1,6 @@
+let info = navigator.userAgent;
+document.getElementById("info").innerHTML = "Navigateur: " + info
+let modif = new Date;
+document.getElementById("date").innerHTML = "Modification: " + modif 
+let titre = document.title
+document.getElementById("titre").innerHTML = "Le titre est:" + titre
